b033e7c945ea49c05f9aeee7a69c770bc5e98d92
dean <dean@deannolan.co.uk>
2017-04-07 23:37:16 +0100

    Don't show deposit popup message when withdrawal made. (temp fix that should be taken care of on the backend)

--- src/store.js
+++ src/store.js
@@ -257,7 +257,9 @@ const actions = {
     socketHub.on('newDeposit', (balance, amount, currency, appId) => {
       if (Settings.AppId === appId) {
         commit(types.SET_BALANCE, {Balance: balance, Currency: currency});
-        toastr.info(`New deposit of ${amount} ${currencies[currency].code} received`);
+        if (amount > 0) {
+          toastr.info(`New deposit of ${amount} ${currencies[currency].code} received`);
+        }
       }
     });
 
